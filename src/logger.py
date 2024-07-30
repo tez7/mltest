d42de5f1718b51a46046d83277851c6d82eb45c4
tez7 <7antez@gmail.com>
2024-07-30 14:32:31 +0530

    third commit add various file

---
+++ src/logger.py
@@ -0,0 +1,21 @@
+import logging
+import os
+from datetime import datetime
+
+# Create a log file name with the current datetime
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+# Create the logs directory path
+logs_path = os.path.join(os.getcwd(), "logs",LOG_FILE)
+os.makedirs(logs_path, exist_ok=True)
+
+# Create the full log file path
+LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+
+# Configure logging
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
