752aacc1deb9631924ab7d43622cdc2deba31564
tez7 <7antez@gmail.com>
2024-07-31 17:23:44 +0530

    eight commit hyperperameter tunning compleyted

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ from src.exception import CustomException
 from src.logger import logging
 import dill
 from sklearn.metrics import r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path,obj):
     try:
@@ -21,14 +21,21 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
 
-            model.fit(X_train, y_train) # Train model
+            gs = GridSearchCV(model, param, cv = 3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_) # select best params
+            model.fit(X_train, y_train) # train on best params
+
+            #model.fit(X_train, y_train) # Train model
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
